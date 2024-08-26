67c05620d695e8d241e9872856462d81fc7fb861
Praneet Dixit <praneetdixit1234@gmail.com>
2024-08-26 23:26:34 +0530

    Update as per format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,47 @@
-function setNav(){if(window.pageYOffset>70){document.getElementById("navHeader").classList.add("scrolled");document.getElementById("toTop").classList.remove("d-none");}else{document.getElementById("navHeader").classList.remove("scrolled");document.getElementById("toTop").classList.add("d-none");}}document.addEventListener('aos:in:startAnim',()=>{setTimeout(function(){document.querySelectorAll(".ind div").forEach(function(elem,ind){elem.style.animationDelay = `${ind*500}ms`;elem.style.animationName = "bg";});}, 1000);});document.getElementById("toTop").addEventListener("click",function(){window.scrollTo(0, 0);});function navbarlinksActive(){let navbarlinks = document.querySelectorAll(".nav-link");let position = window.scrollY + 200;navbarlinks.forEach(function(item){let section = document.querySelector(item.hash);if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)){item.parentElement.classList.add('current');}else{item.parentElement.classList.remove('current');}});}window.onscroll=function(){navbarlinksActive();setNav();};document.getElementById("togNav").addEventListener("click", function(){document.getElementById("toExpandNav").classList.toggle("show");});let form = document.getElementsByTagName("form")[0];form.addEventListener('submit',function(event){if(form.checkValidity() === false){event.preventDefault();event.stopPropagation();}form.classList.add('was-validated');});
+function setNav() {
+  if (window.scrollY > 70) {
+    document.getElementById("navHeader").classList.add("scrolled");
+    document.getElementById("toTop").classList.remove("d-none");
+  } else {
+    document.getElementById("navHeader").classList.remove("scrolled");
+    document.getElementById("toTop").classList.add("d-none");
+  }
+}
+document.addEventListener("aos:in:startAnim", () => {
+    document.querySelectorAll(".ind div").forEach(function (elem, ind) {
+      elem.style.animationName = "bg";
+    });
+});
+document.getElementById("toTop").addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+function navbarlinksActive() {
+  let navbarlinks = document.querySelectorAll(".nav-link");
+  let position = window.scrollY + 200;
+  navbarlinks.forEach(function (item) {
+    let section = document.querySelector(item.hash);
+    if (
+      (position >= section.offsetTop) &&
+      (position <= section.offsetTop + section.offsetHeight)
+    ) {
+      item.parentElement.classList.add("current");
+    } else {
+      item.parentElement.classList.remove("current");
+    }
+  });
+}
+window.onscroll = function () {
+  navbarlinksActive();
+  setNav();
+};
+document.getElementById("togNav").addEventListener("click", function () {
+  document.getElementById("toExpandNav").classList.toggle("show");
+});
+let form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", function (event) {
+  if (form.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  form.classList.add("was-validated");
+});
